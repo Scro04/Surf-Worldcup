7136a05496010e4380803005e150076e720250f5
meister92 <herbert.fuchs@student.tugraz.at>
2015-06-02 15:39:16 +0200

    [HF, AS] [Competition] Design Changes
    Change Textfield and Button Positions

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -167,7 +167,8 @@ angular.module('starter.controllers', [])
 
 .controller('pwaCtrl', function ($scope) {
   
-  $scope.header = bewerbeDataSet_[1][1];
+  $scope.header = bewerbeDataSet_[1][1].split("-");
+  
   $scope.descr = bewerbeDataSet_[1][4];
     
   //------------------------- Rider -----------------------------	
@@ -182,20 +183,6 @@ angular.module('starter.controllers', [])
   }
   $scope.riderData = tmp_array;
 
-  var button = document.getElementById('rider_button');  
-  var compare = "open";
-  $(document).ready(function () {
-    $('#rider_button').click(function () {
-        $('.rider').slideToggle("fast");
-        if (compare == "open") {
-          button.className = "button icon-right ion-chevron-down";
-          compare = "close";
-        } else {
-          button.className = "button icon-right ion-chevron-right";
-          compare = "open";
-        }
-    });
-  });
   //------------------------- Program -----------------------------
   var tmp_array_prog = []; 
     
@@ -206,30 +193,157 @@ angular.module('starter.controllers', [])
       tmp_array_prog.push(programmDataSet_[x]);    
     }
   }
+  
+  tmp_array_prog.sort(sortFunction); 
+  
+  function sortFunction(a, b) {
+    if (a[1] === b[1]) {
+        return 0;
+    }
+    else {
+        return (a[1] < b[1]) ? -1 : 1;
+    }
+  }
+  
   $scope.programData = tmp_array_prog;
   
-  
+  //------------------------- Buttons -----------------------------
+  var is_prog_open = 0;
+  var is_rider_open = 0;
   var button1 = document.getElementById('program_button');  
-  var compare1 = "open";
+  var button = document.getElementById('rider_button');  
+  var compare = "open";
+  var compare1 = "open1";
+  
   $(document).ready(function () {
-    $('#program_button').click(function () {
-        $('.program').slideToggle("fast");
-        if (compare1 == "open") {
-          button1.className = "button icon-right ion-chevron-down";
-          compare1 = "close";
-        } else {
+    $('#rider_button').click(function () {
+        $('.rider').slideToggle("fast");
+        if(is_prog_open)
+        {
+          $('.program').slideToggle("fast");
           button1.className = "button icon-right ion-chevron-right";
-          compare1 = "open";
+          compare1 = "open1";
+          is_prog_open = 0;    
+        }
+      
+        if (compare == "open") {
+          button.className = "button icon-right ion-chevron-down";
+          compare = "close";
+          is_rider_open = 1;
+        } else {
+          button.className = "button icon-right ion-chevron-right";
+          compare = "open";
+          is_rider_open = 0;
         }
     });
   });
+ 
+  $(document).ready(function () {
+    $('#program_button').click(function () {
+        $('.program').slideToggle("fast");
+        if(is_rider_open)
+        {
+          $('.rider').slideToggle("fast");
+          button.className = "button icon-right ion-chevron-right";
+          compare = "open";
+          is_rider_open = 0;    
+        } 
+        if (compare1 == "open1") {
+          button1.className = "button icon-right ion-chevron-down";
+          compare1 = "close1";
+          is_prog_open = 1;
+        } else {
+          button1.className = "button icon-right ion-chevron-right";
+          compare1 = "open1";
+          is_prog_open = 0;
+        }
+    });
+  });
+  
+  //-------- calendar ---------------
+
+  var q = new Date();
+  var m = q.getMonth()+1;
+  var d = q.getDay();
+  var y = q.getYear();
+
+  var pic_name = [];
+  
+  console.log(m);
+  
+  for(var i = 0; i < tmp_array_prog.length; i++)
+  {
+    var name;
+    var split_ = tmp_array_prog[i][1].split("-");
+    if(y <= split_[0])
+    {  
+      if(m <= split_[1])
+      {
+        if(d <= split_[2]) 
+          {
+             name = "img/date/" + split_[2] + "n.jpg";
+          }
+        else
+           name = "img/date/" + split_[2] + ".jpg";
+      }
+      else             
+        name = "img/date/" + split_[2] + ".jpg";
+    }
+    else      
+      name = "img/date/" + split_[2] + ".jpg";
+    
+    pic_name.push(name);
+  }
+  $scope.picName = pic_name;
+
+  
+  //--------------------- Show More ---------------
+  
+  $(document).ready(function() {
+    var showChar = 300;
+    var ellipsestext = "...";
+    var moretext = "more";
+    var lesstext = "less";
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar-1, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ ' </span><span class="morecontent"><span>' + h + '</span>  <a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+  
+  
+  
+  
+
 })
 
 
 //Competition Controller
 .controller('compCtrl', function ($scope) {
 	
-	console.log(riderDataSet_);
+	//console.log(riderDataSet_);
  
  /* var table_size = document.getElementById('table').rows.length;
   
